f744df413077ed3d3a9666ecd9e350f2004419ca
Kaue-G <kaueguedes.dev@gmail.com>
2021-05-03 11:59:22 -0300

    concluido

--- filterableImgGallery/script.js
+++ filterableImgGallery/script.js
@@ -22,4 +22,38 @@ window.onload = () =>{ //once window loaded
             });
         }
     }
+
+    for (let index = 0; index < filterImg.length; index++) {
+        filterImg[index].setAttribute("onclick", "preview(this)"); //adding onclick attribute in all avaliabel images
+    }
+}
+
+// selecting all required elements
+
+const previewBox = document.querySelector(".preview-box"),
+previewImg = previewBox.querySelector("img"),
+categoryName = previewBox.querySelector(".title p"),
+closeIcon = previewBox.querySelector(".icon"),
+shadow = document.querySelector(".shadow");
+
+// fullScreen preview image function
+
+function preview(element){
+    // once user click on any image then remove the scrollbar of the body, so user cant scroll up ou down
+    document.querySelector("body").style.overflow = "hidden";
+
+    let selectedPrevImg = element.querySelector("img").src; // getting user clicked image source link and store in a variable
+    let selectedImgCategory = element.getAttribute("data-name"); // getting user clicked data-name value
+    categoryName.textContent = selectedImgCategory; // passing the data-name value to category name variable
+
+    previewImg.src = selectedPrevImg; // passing the user clicked image source in the preview image source
+
+
+    previewBox.classList.add("show"); // show the preview box
+    shadow.classList.add("show"); // show the light grey background
+    closeIcon.onclick = () =>{ // if user click on the close icon of the preview box
+        previewBox.classList.remove("show"); // hide the preview box
+        shadow.classList.remove("show"); // hide the grey background
+        document.querySelector("body").style.overflow = "scroll"; // show the scroll bar of body
+    }
 }
